de96270e5e9986d2448822df7fbb01d767bdb9a4
MakeMike <57689923+MakeMike@users.noreply.github.com>
2021-02-15 20:11:13 -0500

    main commit

--- server.py
+++ server.py
@@ -1,14 +1,14 @@
 from flask import Flask, Response, render_template
 from flask_socketio import SocketIO, send
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 app.config['SECRET_KEY'] = 'afsdjk02934857676jdfhgieurnnr8402984hh48hriuausdfjkruioiuweyrtoiuyweritywerthujertgyuewrouisdfhgiushdfiougdfouiphsdrpoiuh'
 socketio = SocketIO(app, cors_allowed_origins='*')
 
-@socketio.on('message')
-def handleMessage(msg):
-	print('Message: ' + msg)
-	send(msg, broadcast=True)
+
+@app.route('/')
+def server():
+	return render_template("msgindex.html")
 if __name__ == '__main__':
-	socketio.run(app)
+	app.run(host='0.0.0.0', port=3000, threaded=True)
 	#host='0.0.0.0', port=3000
